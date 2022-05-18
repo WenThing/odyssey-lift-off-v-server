8230a8e2525b74edc8136747ba862cd7bae4f7f7
Wen Thing <ngwenthing@sains.com.my>
2022-05-18 08:31:20 +0800

    Switch to async function for server start & set PORT listening

--- src/index.js
+++ src/index.js
@@ -3,20 +3,28 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI()
-    }
-  }
-});
+// use async & await to start server
+async function startApolloServer(typeDefs, resolvers) {
+  
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI()
+      };
+    },
+  });
 
-server.listen().then(() => {
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+  
   console.log(`
       🚀  Server is running!
-      🔉  Listening on port 4000
-      📭  Query at https://studio.apollographql.com/dev
+      🔉  Listening on port ${port}
+      📭  Query at ${url};
     `);
-});
+
+}
+
+startApolloServer(typeDefs, resolvers);
+
